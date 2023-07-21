cdc8df0cb9025c37c990dcefac67f220aa871234
Cecilia <45336766+ceciliadean182@users.noreply.github.com>
2023-07-21 23:43:59 +0000

    Upload

--- src/app.js
+++ src/app.js
@@ -6,6 +6,46 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#excuse").innerHTML = excuseGenerator();
+  });
 };
+
+function excuseGenerator() {
+  let pronombre = ["Un", "El", "Mi"];
+  let persona = [
+    "lobo",
+    "gato",
+    "conductor de bondi",
+    "madre",
+    "abuela",
+    "vecino"
+  ];
+  let accion = ["comió", "robó", "mojó", "rompió"];
+  let objeto = ["mi tarea", "mi informe del trabajo", "mi curriculum"];
+  let lugar = [
+    "en la rambla",
+    "en el balcón",
+    "en mi casa",
+    "en su casa",
+    "en el edificio"
+  ];
+
+  let pronombreIndex = Math.floor(Math.random() * pronombre.length);
+  let personaIndex = Math.floor(Math.random() * persona.length);
+  let accionIndex = Math.floor(Math.random() * accion.length);
+  let objetoIndex = Math.floor(Math.random() * objeto.length);
+  let lugarIndex = Math.floor(Math.random() * lugar.length);
+
+  return (
+    pronombre[pronombreIndex] +
+    " " +
+    persona[personaIndex] +
+    " " +
+    accion[accionIndex] +
+    " " +
+    objeto[objetoIndex] +
+    " " +
+    lugar[lugarIndex]
+  );
+}
